7f11dd727135b9dc2f2ef72aef3f47d15b8e0848
Sushma Unnibhavi <sushmaunnibhavi@gmail.com>
2018-02-18 13:34:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -79,18 +79,11 @@ app.get('/', function(req, res){
 });
 
 app.get('/:articleName',function(req,res){
+    //articleName==article-one
     var articleName=req.params.articleName;
-  res.sendFile(createTemplate(articles[articleName]));  
+  res.send(createTemplate(articles[articleName]));  
 });
-app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname,'ui', 'article-one.html'));
-});
-app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname,'ui', 'article-two.html'));
-});
-app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname,'ui', 'article-three.html'));
-});
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
