a4418fd8f277230c92bac89e00d2133e9b977b1c
Sushma Unnibhavi <sushmaunnibhavi@gmail.com>
2018-02-18 21:14:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,13 @@ console.log('Loaded!');
 var element= document.getElementById('main-text');
 element.innerHTML= 'New Value';
 var img=document.getElementById('madi');
+var marginLeft=0;
+function moveRight()
+{
+    marginLeft=marginLeft+10;
+    img.style.marginLeft= marginLeft+'px';
+}
 img.onclick=function(){
-  img.style.marginLeft='100px';  
+    var interval= setInterval(moveRight,100);
+   
 };
