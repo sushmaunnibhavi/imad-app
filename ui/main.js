d3a0d8faa161f5c60fc70350f22806d893853d4e
Sushma Unnibhavi <sushmaunnibhavi@gmail.com>
2018-02-19 19:41:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,3 +15,20 @@ button.onclick=function(){
   request.open('GET','http://sushmaunnibhavi.imad.hasura-app.io/counter',true);
   request.send(null);
 };
+var nameInput= document.getElementById('name');
+var name= nameInput.value;
+var submit= document.getElementById('submit_btn');
+submit.onclick =function(){
+    
+    
+    
+    
+    
+    var names=['name1','name2','name3','name4'];
+    var list='';
+    for(var i=0; i<names.length;i++){
+        list=='<li>'+names[i]+'</li>';
+    }
+   var ul=document.getElementById('namelist');
+   ul.innerHTML=list;
+};
