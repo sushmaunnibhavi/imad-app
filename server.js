6f43118c845884f8b9c8191f7f3276fa47d4aef5
Sushma Unnibhavi <sushmaunnibhavi@gmail.com>
2018-02-24 07:29:39 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -110,10 +110,25 @@ app.get('/submit-name',function(req,res){
     res.send(JSON.stringify(names));
 });
 
-app.get('/:articleName',function(req,res){
+app.get('/articles/:articleName',function(req,res){
     //articleName==article-one
-    var articleName=req.params.articleName;
-  res.send(createTemplate(articles[articleName]));  
+    
+    pool.query("SELECT * FROM article WHERE title=" +req.params.articleName,function(err,result){
+       if(err){
+           res.status(500).send(err.toString());
+       }else{
+           if(result.rows.length===0){
+               res.status(404).send('Article not found');
+           }
+           else{
+               var articleData=result.row[0];
+                res.send(createTemplate(articleData));
+           }
+       }
+    });
+    
+    
+   
 });
 
 
