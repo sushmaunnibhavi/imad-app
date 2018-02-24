2dfe3e89bc0eb153f9d661e264ba27cbdae7171b
Sushma Unnibhavi <sushmaunnibhavi@gmail.com>
2018-02-24 05:40:09 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var pool= require('pg').Pool;
+var config{
+    user: 'sushmaunnibhavi',
+    database: 'sushmaunnibhavi',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password:process.env DB_PASSWORD
+};
 var app = express();
 app.use(morgan('combined'));
 var articles={
@@ -77,6 +84,19 @@ return htmlTemplate;
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname,'ui', 'index.html'));
 });
+var pool= new Pool.config;
+app.get('/test-db',function(req,res){
+  //make a select request
+  //return a response with the results
+  pool.query('SELECT * FROM test',function(err,result){
+     if(err){
+         res.status(500).send(err.toString());
+     } else
+     res.send(JSON.stringify(result));
+  });
+});
+
+
 var counter=0;
 app.get('/counter',function(req,res){
     counter=counter+1;
