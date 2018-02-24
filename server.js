78da23903cfd28af0efe642d15b28b2529ace359
Sushma Unnibhavi <sushmaunnibhavi@gmail.com>
2018-02-24 07:50:18 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -113,7 +113,7 @@ app.get('/submit-name',function(req,res){
 app.get('/articles/:articleName',function(req,res){
     //articleName==article-one
     
-    pool.query("SELECT * FROM article WHERE title='" +req.params.articleName+"'",function(err,result){
+    pool.query("SELECT * FROM article WHERE title=$1" ,[req.params.articleName],function(err,result){
        if(err){
            res.status(500).send(err.toString());
        }else{
