58d4a366186c46bc93c742f58a7ac11c75f573f5
Sushma Unnibhavi <sushmaunnibhavi@gmail.com>
2018-02-24 21:15:31 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ return htmlTemplate;
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname,'ui', 'index.html'));
 });
-var pool= newPool(config);
+var pool= new Pool(config);
 app.get('/test-db',function(req,res){
   //make a select request
   //return a response with the results
