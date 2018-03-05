8ddff0753930e621041f501119ad5f7c7c2544a0
Sushma Unnibhavi <sushmaunnibhavi@gmail.com>
2018-03-05 08:00:33 -0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,39 +1,25 @@
-var button= document.getElementById('counter');
-var counter=0;
 
-button.onclick=function(){
-  var request= new XMLHttpRequest();
-  request.onreadystatechange=function(){
-      if (request.readyState===XMLHttpRequest.DONE){
-          if(request.status===200){
-              var counter=request.responseText;
-              var span= document.getElementById('count');
-              span.innerHTML=counter.toString();
-          }
-      }
-  };
-  request.open('GET','http://sushmaunnibhavi.imad.hasura-app.io/counter',true);
-  request.send(null);
-};
-var nameInput= document.getElementById('name');
-var name= nameInput.value;
 var submit= document.getElementById('submit_btn');
 submit.onclick =function(){
     var request=new XMLHttpRequest();
     request.onreadystatechange=function(){
         if(request.readyState===XMLHttpRequest.DONE){
             if(request.status===200){
-                 var names=request.responseText;
-                 names=JSON.parse(names);
-    var list='';
-    for(var i=0; i<names.length;i++){
-        list=='<li>'+names[i]+'</li>';
-    }
-   var ul=document.getElementById('namelist');
-   ul.innerHTML=list;
+                console.log('user logged in');
+                alert('logged in successfully');
+            }else if(request.status===403){
+                alert('username/password is incorrect');
+                
+            }else if(request.status===500){
+                alert('something went wrong on the server');
             }
         }
 };
-request.open('GET',http://sushmaunnibhavi.imad.hasura-app.io/submit-name?name='+name,true);
-request.send(null);
+var username=document.getElemenyNyId('username').value;
+var password=document.getElemenyNyId('password').value;
+console.log(username);
+console.log(password);
+request.open('POST','http://sushmaunnibhavi.imad.hasura-app.io/login',true);
+request.setRequestHeader('Content-type','application/json');
+request.send(JSON.stringify({username:username,password:password}));
 };
