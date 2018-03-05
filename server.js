d2a960aca55f0946933dbcffdab8db6c28090989
Sushma Unnibhavi <sushmaunnibhavi@gmail.com>
2018-03-05 05:39:23 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var pool= require('pg').Pool;
-
+var crypto=require('crypto');
 var config={
     user: 'sushmaunnibhavi',
     database: 'sushmaunnibhavi',
@@ -55,6 +55,25 @@ return htmlTemplate;
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname,'ui', 'index.html'));
 });
+
+function hash(input,salt){
+    var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+    return hashed;
+}
+app.get('/hash/:input',function(req,res){
+    var hashedString=hash(req.params.input,'this-is-some-random-string');
+    res.send(hashedString).toString('hex');
+
+});
+
+
+
+
+
+
+
+
+
 var pool= new Pool(config);
 app.get('/test-db',function(req,res){
   //make a select request
