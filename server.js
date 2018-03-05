cb0f9e54509d83f9e90057f364a9365d82b498e9
Sushma Unnibhavi <sushmaunnibhavi@gmail.com>
2018-03-05 06:11:12 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var pool= require('pg').Pool;
 var crypto=require('crypto');
+var bodyParser=require('body-parser');
 var config={
     user: 'sushmaunnibhavi',
     database: 'sushmaunnibhavi',
@@ -12,6 +13,7 @@ var config={
 };
 var app = express();
 app.use(morgan('combined'));
+app,use(bodyParser.JSON());
 
 
 function createTemplate(data){
@@ -66,7 +68,19 @@ app.get('/hash/:input',function(req,res){
 
 });
 
-
+app.post('/create-use',function(req,res){
+    var username=req.body.username;
+    var password=req.body.password;
+    var salt=crypto.getRandomBytes(128).toString('hex');
+ var dbString=hash(password,salt);
+ pool.query('INSERT INTO "user"(username,password) VALUES($1,$2)',[username,dbString],function(err,result){
+     if(err){
+         res.status(500).send(err.toString());
+     } else{
+     res.send('User successfully created:'+username);
+     }
+ });
+});
 
 
 
